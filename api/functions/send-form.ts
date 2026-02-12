f38f60080169e1cd93ed3f93096a6db43f4f2ed5
Miguel Gallego <1636242@uab.cat>
2026-02-12 11:23:31 +0100

    Add debug on send-form

--- api/functions/send-form.ts
+++ api/functions/send-form.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { verify } from 'jsonwebtoken';
 import axios from 'axios';
-import {parse} from 'cookie';
+import { parse } from 'cookie';
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 const BOT_TOKEN = process.env.BOT_TOKEN!;
@@ -11,6 +11,12 @@ const WORKFLOW_FILE_NAME = 'update-db.yml';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 
+
+    //Debug
+    if (req.method === "GET") {
+        return res.status(200).json({ whoami: "SEND-FORM MIGUEL" });
+    }
+
     //0 - Allow CORS and verify POST
     const origin = "https://erilllab.github.io" //change in dev
 
@@ -18,14 +24,14 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  
+
     if (req.method === "OPTIONS") {
-      return res.status(200).end(); // CORS preflight
+        return res.status(200).end(); // CORS preflight
     }
 
 
     console.log("Calling send-form API");
-    
+
 
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Only POST allowed' });
@@ -33,7 +39,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     //1 - Verify JWT token from cookies
     console.log("Cookie: ", req.headers.cookie);
-    
+
     const cookies = parse(req.headers.cookie || '');
     const token = cookies['session_token'];
     console.log("Session token: ", token);
@@ -51,7 +57,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const { inputs } = req.body;
 
     console.log("Inputs: ", inputs);
-    
+
 
     //3 - Dispatch GitHub workflow via GitHub API
 
@@ -64,17 +70,17 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
             },
             {
                 headers: {
-                Authorization: `Bearer ${BOT_TOKEN}`,
-                Accept: 'application/vnd.github+json',
+                    Authorization: `Bearer ${BOT_TOKEN}`,
+                    Accept: 'application/vnd.github+json',
                 },
             }
         );
 
         return res.status(200).json({ message: 'Workflow dispatched' });
     } catch (err: any) {
-          const status = err.response?.status || 500;
-          const data = err.response?.data || { message: err.message };
-          console.error("Error dispatching:", status, data);
-          return res.status(500).json({ error: "SEND-FORM ERROR" });
+        const status = err.response?.status || 500;
+        const data = err.response?.data || { message: err.message };
+        console.error("Error dispatching:", status, data);
+        return res.status(500).json({ error: "SEND-FORM ERROR" });
     }
 }
