82349c25ef456e79eb6c3153789709bc3eed35e6
Ivan Erill <ivanerill@users.noreply.github.com>
2026-09-18 18:26:16 +0200

    Update consts.ts
    fixed auth issue

--- consts.ts
+++ consts.ts
@@ -3,5 +3,5 @@
 //export const REPO_OWNER_GLOBAL = "alexHernandezCortacans"
 
 // To change before merge to prod
-export const origin = "https://collectf.org";
+export const originConstGlobal = "https://collectf.org";
 const REPO_OWNER = "ErillLab";
